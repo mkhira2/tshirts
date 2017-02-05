a6c945d84d985d48b900d06d4d3baf040e90e422
Kenji Hirabayashi <mkhira2@gmail.com>
2017-02-05 09:38:48 -0600

    minor text change

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,8 @@
 function popup() {
 	var age = prompt('WARNING: Back of shirt is NSFW. Must be 18 or older to view. Please enter your age: ');
 	if (age >= 18) {
-		alert("Your body may be of age, but your mind still seems childish. Grow up and try again.");
+		alert("Your body may be of age, but your mind still seems childish. No flip for you.");
 	} else if (age < 18) {
-		alert("Not old enough, it seems. Go do your homework and quit browsing adult websites.");
-	} else {
+		alert("Not old enough, it seems. Try again in "  + (18 - age) +  " years.");
 	}
 }
